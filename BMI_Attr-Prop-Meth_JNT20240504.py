3224c87da9245ee377aa1b0b03a8de6ab8fc1b37
陳添盛 <chen.johnathan@gmail.com>
2024-05-18 08:22:39 +0000

    0

---
+++ BMI_Attr-Prop-Meth_JNT20240504.py
@@ -0,0 +1,49 @@
+#class BMI
+#attribute -> name,weight,height
+#property -> bmi
+#instance method -> status
+#輸入
+#請輸入姓名
+#體重
+#身高
+#輸出
+#徐國堂您好,BMI:26.12,體重:過重
+
+class BMI():
+    def __init__(self,n:str):
+        self.name = n
+    
+#    def __repr__(self):
+#        return f"這是Person的實體,我的名字是{self.name}" #字串插補
+    
+class Human(BMI):
+    def __init__(self,name:str,weight:int,height:int):
+        super().__init__(name)
+        self.__weight = weight
+        self.__height = height
+    
+    @property
+    def weight(self):
+        return self.__weight
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    def __repr__(self):
+        message = ""
+        message += f"姓名:{self.name}\n"
+        message += f"國文:{self.weight}\n"
+        message += f"英文:{self.height}\n"
+        return message
+    
+    def bmi(self) -> float: #實體的method
+        return self.weight / ((self.height/100)**2)
+    
+
+s1 = Human("Johnathan",158,79)
+#print(type(s1))
+print(s1.name)
+print(f"BMI:{s1.bmi()}")
+#print(f"平均:{round(s1.average(),ndigits=2)}")
+print(s1)
